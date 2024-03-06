aa16d58596746cb9f084758fe2b552ea68b92b0b
brechtarnou <brecht.arnou@gmail.com>
2024-03-06 21:04:58 +0100

    cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./user");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
